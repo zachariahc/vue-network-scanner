152ebd00d2e18247a7224c534e402b1fa3c6fa42
Zachariah Crowell <zachariah.crowell@techtonic.com>
2019-10-24 08:14:35 -0600

    Removed internet-available package

--- server/index.js
+++ server/index.js
@@ -1,82 +1,78 @@
-const internetAvailable = require('internet-available')
-const si = require('systeminformation');
-const wifi = require('node-wifi')
+const si = require("systeminformation");
+const wifi = require("node-wifi");
 
-var cors = require('cors');
+var cors = require("cors");
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 app.use(bodyParser());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send({ responseMessage: "Hello From Network Scanner Server" });
+});
 
-app.get('/', (req, res) => {
-    res.send({responseMessage: 'Hello From Network Scanner Server'})
-})
-
-app.get('/networkscanone', async(req, res) => {
-
-   let networkData =  si.wifiNetworks()
+app.get("/networkscanone", async (req, res) => {
+  let networkData = si
+    .wifiNetworks()
     .then(data => {
-        return data
+      return data;
     })
     .catch(error => console.error(error));
-    res.send(await networkData)
-})
+  res.send(await networkData);
+});
 
-app.get('/networkscantwo', async (req, res) => {
-     wifi.init({
-        iface: null 
-      });
-       
-     const networkData =  wifi.scan()
-     .then(data => {
-         return data
-     })
-     .catch(err => console.error(err))
-      res.send(await networkData)
-      res.end()
-})
+app.get("/networkscantwo", async (req, res) => {
+  wifi.init({
+    iface: null
+  });
 
-app.get('/currentconnection', async (req,res) => {
-
-    wifi.init({
-        iface: null
-      });
-
-    const currentConnection = wifi.getCurrentConnections()
+  const networkData = wifi
+    .scan()
     .then(data => {
-        return data
+      return data;
     })
-      res.send(await currentConnection)
-      res.end()
-})
+    .catch(err => console.error(err));
+  res.send(await networkData);
+  res.end();
+});
 
-app.post('/connect', async (req,res) => {
+app.get("/currentconnection", async (req, res) => {
+  wifi.init({
+    iface: null
+  });
 
-    if(!req.body){
-        res.send({message: "No body sent"})
-        return
-    }
-    
-    console.log(req.body)
-    wifi.init({
-        iface: null
-      });
-    const connection = wifi.connect({ ssid: req.body.ssid, password: req.body.password })
+  const currentConnection = wifi.getCurrentConnections().then(data => {
+    return data;
+  });
+  res.send(await currentConnection);
+  res.end();
+});
+
+app.post("/connect", async (req, res) => {
+  if (!req.body) {
+    res.send({ message: "No body sent" });
+    return;
+  }
+
+  console.log("Body of fetch request from front end", req.body);
+  wifi.init({
+    iface: null
+  });
+  const connection = wifi
+    .connect({ ssid: req.body.ssid, password: req.body.password })
     .then(data => {
-        return {message: "Connection successful", data}
+      return { message: "Connection successful", data };
     })
     .catch(err => {
-        return { message: "Sorry there was a problem connecting", err }
-    })
-    res.send(await connection)
-    res.end()
-})
+      return { message: "Sorry there was a problem connecting", err };
+    });
+  res.send(await connection);
+  res.end();
+});
 
-    app.listen(port, () => console.log(`Server on wayne ${port}!`))
-
+app.listen(port, () => console.log(`Server on wayne ${port}!`));
